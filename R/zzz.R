f0d865ca43af613c33372604ded7a929636358aa
Fridolin Linder <fridolin.linder@nyu.edu>
2019-02-20 11:29:19 -0500

    Update zzz.R

--- R/zzz.R
+++ R/zzz.R
@@ -1,11 +1,11 @@
 .onLoad <- function(libname, pkgname) {
-    source('tools/make_cc.R')
-    stanfiles = list.files('src/stan_files/', pattern = '.stan')
-    setwd('src/')
-    for(f in stanfiles) {
-        make_cc(paste0('stan_files/', f))
-    }
-    setwd('../')
+    #source('tools/make_cc.R')
+    #stanfiles = list.files('src/stan_files/', pattern = '.stan')
+    #setwd('src/')
+    #for(f in stanfiles) {
+    #    make_cc(paste0('stan_files/', f))
+    #}
+    #setwd('../')
     modules <- paste0("stan_fit4", names(stanmodels), "_mod")
     for (m in modules) loadModule(m, what = TRUE)
 }
