6afb8be9ac77642f8d2ee02551b6882cc08586de
Gregory Eady <gregory.eady@gmail.com>
2019-02-22 15:44:01 -0500

    added DGP sim and .stan model

--- R/simulate_dgp.R
+++ R/simulate_dgp.R
@@ -9,104 +9,128 @@ library(scales)
 library(parallel)
 library(matrixStats)
 library(MASS)
+library(invgamma)
 
 ##### SET WORKING DIRECTORY (IF ON PERSONAL COMPUTER OR HPC)
 if(grepl("gregoryeady", system("whoami", intern = TRUE))) {
-  setwd("/Users/GregoryEady/Dropbox (Personal)/Code/URL_Scaling/")
+  setwd("/Users/GregoryEady/GitHub/mediascores")
 } else if(grepl("ge31", system("whoami", intern = TRUE))) {
-  setwd("/scratch/ge31/URL_Scaling")
+  setwd("/scratch/ge31/GitHub/mediascores")
 }
 
 n_cores <- 4
 options(mc.cores = n_cores)
 
-n_users <- 250
-n_domains <- 100
-affiliation <- rbinom(n_users, 1, 0.6) + 1
+n_users <- 200
+n_domains <- 500
 
-lambda <- 1
+# Group that users might belong to (e.g. Republican, Democrat, ordinary user)
+# Can input "1" for all users if all belong to same group or no grouping desired
+group <- rbinom(n_users, 1, 0.6) + 1
 
-alpha_mu <- 1.5
+# alpha is the user-level intercept
+# Represents the baseline degree to which a given user shares news URLs
+# in general
+alpha_mu <- 2
 alpha_sigma <- 0.5
 alpha <- rnorm(n_users, mean = alpha_mu, sd = alpha_sigma)
 
+
+# gamma is the domain-level intercept
+# Represents the baseline degree to which a users shares news URLs from a given 
+# news domain in general. Will be a large value for often-shared domains
+# (e.g. nytimes.com) and low for rarely-shared domains (e.g. stripes.com)
+# No gamma_mu because is unidentified (alpha_mu acts as the model intercept)
 gamma_sigma <- 0.75
 gamma <- rnorm(n_domains, mean = 0, sd = gamma_sigma)
 
+# theta is the ideology of each user
+# theta is given a separate hierarchical prior defined by a user's group
+# i.e. the "group" variable defined above
 theta_mu <- c(-0.75, 0.5)
 theta_sigma <- c(0.25, 0.25)
-theta <- numeric(length(affiliation))
+theta <- numeric(length(group))
 
-theta[affiliation == 1] <- rnorm(sum(affiliation == 1),
-                                 mean = theta_mu[1], sd = theta_sigma[1])
+theta[group == 1] <- rnorm(sum(group == 1),
+                           mean = theta_mu[1], sd = theta_sigma[1])
 
-theta[affiliation == 2] <- rnorm(sum(affiliation == 2),
-                                 mean = theta_mu[2], sd = theta_sigma[2])
+theta[group == 2] <- rnorm(sum(group == 2),
+                           mean = theta_mu[2], sd = theta_sigma[2])
 
-zeta_sigma <- 0.5
-zeta <- rnorm(n_domains, mean = 0, sd = zeta_sigma)
-zeta[1] <- -1
-zeta[2] <- +1
 
-# a <- 4
-# b <- 3
-# omega <- 1/rgamma(n_users, a, b)
+# zeta is the ideology of each news domain
+# Identification in the model is fixed by setting the prior on these parameters
+# to Normal(0, 1)
+zeta_sigma <- 1
+zeta <- rnorm(n_domains, mean = 0, sd = 1)
 
+# To define the polarization the scale (whether high values denote liberal or
+# conservative), we assume that the research has in mind a well-known liberal
+# domain and a well-known conservative domain.
+# For simulation, we'll define the anchors as the domains with the lowest and
+# highest theta
+anchors <- c(which(zeta == min(zeta)), which(zeta == max(zeta)))
+
+# omega_user and omega_domain represent the variance of the model
+a_user <- 5
+b_user <- 5
+omega_user <- rinvgamma(n_users, a_user, b_user)
+
+a_domain <- 5
+b_domain <- 5
+omega_domain <- rinvgamma(n_domains, a_domain, b_domain)
+
+# Compute the values of the user-domain count matrix: the count of each domain
+# shared by each user
 Y <- matrix(data = 0, nrow = n_users, ncol = n_domains)
 
 for(i in 1:n_users) {
-    # Y[i, ] <- rnegbin(n_domains, exp(alpha[i] + gamma - lambda * ((theta[i] - zeta)^2)), omega[i])
-    Y[i, ] <- rpois(n_domains, exp(alpha[i] + gamma - lambda * ((theta[i] - zeta)^2)))
+    Y[i, ] <- rnegbin(n_domains, exp(alpha[i] + gamma - ((theta[i] - zeta)^2)), omega_user[i]*omega_domain)
+    # Y[i, ] <- rpois(n_domains, exp(alpha[i] + gamma - ((theta[i] - zeta)^2)))
 }
 
+# Compile the mediascore Stan model
+model_media_scores <- stan_model(file = "src/stan_files/mediascores.stan")
 
-model_media_scores <- stan_model(file = "Media_Scores.stan")
+# N = number of users; M = number of domains; G = number of groups;
+# group = group each user belongs to; Y = user-domain count matrix
+model_data <- list(N = nrow(Y), M = ncol(Y), G = length(unique(group)),
+                   group = group, Y = Y)
 
-model_data <- list(n_users = n_users, n_domains = n_domains,
-                   group = affiliation, Y = Y, verbose = 2)
-
-inits <- rep(list(list(theta = as.numeric(affiliation == 2) - as.numeric(affiliation == 1),
-                       # theta_mu = c(-1, 1),
-                       theta_mu_1 = -1,
-                       theta_mu_2 = 1,
-                       theta_sigma = c(1, 1),
-                       lambda = 1,
-                       alpha = as.numeric(scale(log(apply(Y, 1, sum)))),
-                       gamma = as.numeric(scale(log(apply(Y, 2, sum)))))),
-             n_cores)
-
-model_sim_fitted <- sampling(model_media_scores, data = model_data,
+# Fit the model
+posterior <- sampling(model_media_scores, data = model_data,
                              warmup = 750, iter = 1500,
-                             init = inits, refresh = 50,
-                             open_progress = TRUE, chains = n_cores)
+                             refresh = 50, chains = n_cores,
+                             open_progress = TRUE)
 
-# model_sim_fitted <- vb(model_media_scores, data = model_data, init = inits[[1]],
+# posterior <- vb(model_media_scores, data = model_data,
 #                        output_samples = 5000, tol_rel_obj = 0.001,
 #                        algorithm = "meanfield")
 
 
-plot(alpha, sapply(extract(model_sim_fitted, paste0("alpha[", 1:n_users, "]")), median)); abline(0, 1)
-plot(gamma, sapply(extract(model_sim_fitted, paste0("gamma[", 1:n_domains, "]")), median)); abline(0, 1)
-plot(theta, sapply(extract(model_sim_fitted, paste0("theta[", 1:n_users, "]")), median)); abline(0, 1)
-plot(zeta, sapply(extract(model_sim_fitted, paste0("zeta[", 1:n_domains, "]")), median)); abline(0, 1)
-plot(omega, sapply(extract(model_sim_fitted, paste0("omega[", 1:n_users, "]")), median)); abline(0, 1)
+plot(alpha, sapply(extract(posterior, paste0("alpha[", 1:n_users, "]")), median)); abline(0, 1)
+plot(gamma, sapply(extract(posterior, paste0("gamma[", 1:n_domains, "]")), median)); abline(0, 1)
+plot(theta, sapply(extract(posterior, paste0("theta[", 1:n_users, "]")), median)); abline(0, 1)
+plot(zeta, sapply(extract(posterior, paste0("zeta[", 1:n_domains, "]")), median)); abline(0, 1)
+plot(omega_user, sapply(extract(posterior, paste0("omega_user[", 1:n_users, "]")), median)); abline(0, 1)
+plot(omega_domain, sapply(extract(posterior, paste0("omega_domain[", 1:n_domains, "]")), median)); abline(0, 1)
 
 
 # Check whether 90% of CIs contain true value
-summary(model_sim_fitted, "alpha[1]")
+summary(posterior, "alpha[1]")
 
-mean(c(eta, theta_mu[1], theta_mu[2], theta_sigma[1], theta_sigma[2], zeta_sigma) > sapply(extract(model_sim_fitted, c("eta", "theta_mu[1]", "theta_mu[2]", "theta_sigma[1]", "theta_sigma[2]", "zeta_sigma")), function(x) quantile(x, 0.05)) &
-     c(eta, theta_mu[1], theta_mu[2], theta_sigma[1], theta_sigma[2], zeta_sigma) < sapply(extract(model_sim_fitted, c("eta", "theta_mu[1]", "theta_mu[2]", "theta_sigma[1]", "theta_sigma[2]", "zeta_sigma")), function(x) quantile(x, 0.95)))
+mean(c(eta, theta_mu[1], theta_mu[2], theta_sigma[1], theta_sigma[2], zeta_sigma) > sapply(extract(posterior, c("eta", "theta_mu[1]", "theta_mu[2]", "theta_sigma[1]", "theta_sigma[2]", "zeta_sigma")), function(x) quantile(x, 0.05)) &
+     c(eta, theta_mu[1], theta_mu[2], theta_sigma[1], theta_sigma[2], zeta_sigma) < sapply(extract(posterior, c("eta", "theta_mu[1]", "theta_mu[2]", "theta_sigma[1]", "theta_sigma[2]", "zeta_sigma")), function(x) quantile(x, 0.95)))
 
-mean(alpha > sapply(extract(model_sim_fitted, paste0("alpha[", 1:n_users, "]")), function(x) quantile(x, 0.05)) &
-     alpha < sapply(extract(model_sim_fitted, paste0("alpha[", 1:n_users, "]")), function(x) quantile(x, 0.95)))
-mean(gamma > sapply(extract(model_sim_fitted, paste0("gamma[", 1:n_domains, "]")), function(x) quantile(x, 0.05)) &
-     gamma < sapply(extract(model_sim_fitted, paste0("gamma[", 1:n_domains, "]")), function(x) quantile(x, 0.95)))
-mean(theta > sapply(extract(model_sim_fitted, paste0("theta[", 1:n_users, "]")), function(x) quantile(x, 0.05)) &
-     theta < sapply(extract(model_sim_fitted, paste0("theta[", 1:n_users, "]")), function(x) quantile(x, 0.95)))
-mean(zeta > sapply(extract(model_sim_fitted, paste0("zeta[", 1:n_domains, "]")), function(x) quantile(x, 0.05)) &
-     zeta < sapply(extract(model_sim_fitted, paste0("zeta[", 1:n_domains, "]")), function(x) quantile(x, 0.95)))
-mean(omega > sapply(extract(model_sim_fitted, paste0("omega[", 1:n_users, "]")), function(x) quantile(x, 0.05)) &
-     omega < sapply(extract(model_sim_fitted, paste0("omega[", 1:n_users, "]")), function(x) quantile(x, 0.95)))
+mean(alpha > sapply(extract(posterior, paste0("alpha[", 1:n_users, "]")), function(x) quantile(x, 0.05)) &
+     alpha < sapply(extract(posterior, paste0("alpha[", 1:n_users, "]")), function(x) quantile(x, 0.95)))
+mean(gamma > sapply(extract(posterior, paste0("gamma[", 1:n_domains, "]")), function(x) quantile(x, 0.05)) &
+     gamma < sapply(extract(posterior, paste0("gamma[", 1:n_domains, "]")), function(x) quantile(x, 0.95)))
+mean(theta > sapply(extract(posterior, paste0("theta[", 1:n_users, "]")), function(x) quantile(x, 0.05)) &
+     theta < sapply(extract(posterior, paste0("theta[", 1:n_users, "]")), function(x) quantile(x, 0.95)))
+mean(zeta > sapply(extract(posterior, paste0("zeta[", 1:n_domains, "]")), function(x) quantile(x, 0.05)) &
+     zeta < sapply(extract(posterior, paste0("zeta[", 1:n_domains, "]")), function(x) quantile(x, 0.95)))
+mean(omega > sapply(extract(posterior, paste0("omega[", 1:n_users, "]")), function(x) quantile(x, 0.05)) &
+     omega < sapply(extract(posterior, paste0("omega[", 1:n_users, "]")), function(x) quantile(x, 0.95)))
 
 
---
+++ src/stan_files/mediascores.stan
@@ -0,0 +1,139 @@
+functions {
+
+  vector neg_bin(vector global_par, vector local_par, real[] real_var, int[] int_var) {
+    int map_N = size(int_var);
+    vector[map_N] map_alpha = global_par[1:map_N];
+    vector[map_N] map_theta = global_par[(map_N+1):(map_N*2)];
+    vector[map_N] map_omega_user = global_par[(map_N*2+1):(map_N*3)];
+    real map_gamma = local_par[1];
+    real map_zeta = local_par[2];
+    real map_omega_domain = local_par[3];
+    vector[map_N] lin_pred;
+    real ll = 0;
+
+    for(i in 1:map_N) {
+      lin_pred[i] = map_alpha[i] + map_gamma - square(map_theta[i] - map_zeta);
+    }
+
+    ll = neg_binomial_2_log_lpmf(int_var | lin_pred, map_omega_user * map_omega_domain);
+
+    return [ll]';
+
+  }
+
+}
+
+data {
+
+  int verbose;
+  int N;
+  int M;
+  int G;
+  int group[N];
+  int Y[N, M];
+
+}
+
+transformed data {
+  int<lower = 0> x_i[M, N];
+  real x_r[M, N];
+
+  for (m in 1:M) {
+      x_i[m] = to_array_1d(Y[, m]);
+      x_r[m] = to_array_1d(rep_row_vector(0, N));
+  }
+
+}
+
+parameters {
+
+  // User and domain intercepts
+  vector[N] alpha_raw;         // User intercept
+  real alpha_mu;               // User intercept
+  real<lower = 0> alpha_sigma; // User intercept
+
+  vector[M] gamma_raw;         // Domain intercept
+  real<lower = 0> gamma_sigma; // Domain intercept
+
+  // Variance
+  vector<lower = 0>[N] omega_user; // Dispersion (N or M)
+  real<lower = 0> omega_user_a;    // Dispersion
+  real<lower = 0> omega_user_b;    // Dispersion
+
+  vector<lower = 0>[M] omega_domain; // Dispersion (N or M)
+  real<lower = 0> omega_domain_a;    // Dispersion
+  real<lower = 0> omega_domain_b;    // Dispersion
+
+  // Ideology
+  vector[N] theta_unconstrained_raw; // User ideology
+  vector[G] theta_mu_unconstrained;  // User ideology
+  vector<lower = 0>[G] theta_sigma;  // User ideology
+
+  vector[M] zeta_unconstrained_raw;  // Domain ideology
+  real<lower = 0> zeta_sigma;        // Domain scale
+
+}
+
+transformed parameters {
+  vector[N] alpha = alpha_mu + alpha_raw * alpha_sigma;
+  vector[M] gamma = gamma_raw * gamma_sigma;
+  vector[N] theta_unconstrained;
+  vector[M] zeta_unconstrained = zeta_unconstrained_raw * zeta_sigma;
+  vector[3] gamma_zeta_omega[M];
+
+  for(m in 1:M) {
+    gamma_zeta_omega[m, 1] = gamma[m];
+    gamma_zeta_omega[m, 2] = zeta_unconstrained[m];
+    gamma_zeta_omega[m, 3] = omega_domain[m];
+  }
+  
+  for(i in 1:N) {
+    theta_unconstrained[i] = theta_mu_unconstrained[group[i]] + theta_unconstrained_raw[i] * theta_sigma[group[i]];
+    // theta_unconstrained[i] = theta_unconstrained_raw[i];
+  }
+
+}
+
+model {
+  
+  real C[N, M]; // Count
+
+  alpha_mu ~ normal(0, 2.5);    // User intercept
+  alpha_sigma ~ normal(0, 2.5); // User intercept
+  alpha_raw ~ normal(0, 1);     // User intercept
+
+  gamma_sigma ~ normal(0, 2.5); // Domain intercept
+  gamma_raw ~ normal(0, 1);     // Domain intercept
+  
+  omega_user_a ~ normal(0, 5);
+  omega_user_b ~ normal(0, 5);
+  omega_user ~ inv_gamma(omega_user_a, omega_user_b); // Dispersion
+
+  omega_domain_a ~ normal(0, 5);
+  omega_domain_b ~ normal(0, 5);
+  omega_domain ~ inv_gamma(omega_domain_a, omega_domain_b); // Dispersion
+
+  theta_sigma ~ normal(0, 1);             // User ideology
+  theta_mu_unconstrained ~ normal(0, 1);  // User ideology
+  theta_unconstrained_raw ~ normal(0, 1); // User ideology
+  // theta_unconstrained_raw ~ normal(0, 2.5); // User ideology
+
+  zeta_sigma ~ normal(0, 1);             // Domain ideology
+  zeta_unconstrained_raw ~ normal(0, 1); // Domain ideology
+
+  target += sum(map_rect(neg_bin,
+                         append_row(append_row(alpha, theta_unconstrained), omega_user),
+                         gamma_zeta_omega, x_r, x_i));
+
+}
+
+generated quantities {
+
+  // Fix reflection invariance
+  vector[M] zeta = zeta_unconstrained[1] < zeta_unconstrained[2] ? zeta_unconstrained : zeta_unconstrained * -1;
+  vector[N] theta = zeta_unconstrained[1] < zeta_unconstrained[2] ? theta_unconstrained : theta_unconstrained * -1;
+  vector[G] theta_mu = zeta_unconstrained[1] < zeta_unconstrained[2] ? theta_mu_unconstrained : theta_mu_unconstrained * -1;
+
+
+}
+
